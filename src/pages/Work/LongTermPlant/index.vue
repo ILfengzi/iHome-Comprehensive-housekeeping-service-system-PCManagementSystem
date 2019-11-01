d5035d562e049b3ac5aa2cdde9b7d5fdc8106fe0
HansChoy <791848111@qq.com>
2019-11-01 14:48:31 +0800

    11.1.1

--- src/pages/Work/LongTermPlant/index.vue
+++ src/pages/Work/LongTermPlant/index.vue
@@ -20,8 +20,11 @@
               </template>
             </el-table-column>
           </el-table>
+		  <!-- <el-card header="健康证">
+     
+        	<FileUpload :action='url'></FileUpload>
+      	  </el-card>  -->
 		<el-pagination background @current-change="pageChange" :total="total" :page-size="pageSize" :current-page="pageNum" layout="prev, pager,next,jumper"></el-pagination>
-
 		<el-dialog title="添加员工" :visible.sync="dialogFormVisible">
 			<el-form :model="form">
 				<el-form-item label="员工" :label-width="formLabelWidth" prop="id">
@@ -40,16 +43,43 @@
 				<el-form-item label="工作月数" :label-width="formLabelWidth">
 					<el-input v-model="form.month" autocomplete="off" clearable></el-input>
 				</el-form-item>
+				<el-form-item label="上传相关文件" :label-width="formLabelWidth">
+					<el-upload
+						v-model="form.aboutFile"
+						ref="upload"
+						:action="'/json/longTermOrder/fileUpload'"
+						:limit="1"
+						:file-list="filelist"
+						accept=".png.jpg.gif.jpeg"
+						v-loading="loading"
+						:show-file-list="true"
+						:on-success="onSuccess"
+					>
+						<el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+						
+					</el-upload>
+				</el-form-item>
 			</el-form>
 			<div slot="footer" class="dialog-footer">
 				<el-button @click="dialogFormVisible = false">取 消</el-button>
 				<el-button type="primary" @click="handleUpdate()">确 定</el-button>
 			</div>
    		</el-dialog>
+		<!-- <el-button @click="dialogFormVisible = true"></el-button> -->
 	</div>
 </template>
 <script>
+	import FileUpload from "@/components/FileUpload";
+	
 	export default {
+		 props:{
+			action: {
+				type:String,
+			}
+   		 },
+		components: {
+    		FileUpload
+ 		},
 		name: "longTermPlant",
 		// created() {
     	// 	this.getLongTermOrder(0);
@@ -60,6 +90,8 @@
 		},
 		data() {
 			return {
+				filelist: [],
+      			loading:false,
 				dataSource: [],
 				page: {
 					total: 0,
@@ -87,6 +119,7 @@
 					staffId:null,
 					salary:null,
 					month:null,
+					aboutFile:null,
 				},
 				formLabelWidth: '120px',
 				options:[],
@@ -94,6 +127,7 @@
 				pageSize:6,
 				pageNum:1,
 				total:0,
+				url: "/json/longTermOrder/fileUpload/"
 			}
 		},
 		methods: {
@@ -135,20 +169,28 @@
 				this.orderId=row.id;
 			},
 			handleUpdate(){
+				//this.$refs.upload.submit();
 				this.axios.post('/json/order/addLongOrderDetail',{
 					"orderId":this.orderId,
 					"staffId":this.form.staffId,
 					"salary":this.form.salary,
 					"month":this.form.month,
+					"aboutFile":this.aboutFile
+
 				}
 					).then((res) => {
 					this.dialogFormVisible=false;
-					this.getLongTermOrder(0);
+					this.getLongTermOrder(0,1,this.pageSize);
 					//this.total = res.data.data.total
 				})
+				console.log(this.form);
 			},
 			pageChange(val){
 				this.getLongTermOrder(0,val,this.pageSize);
+			},
+			onSuccess(response, file, fileList){
+				//console.log(response, file, fileList);
+				this.aboutFile=response;
 			}
 		},
 	}
